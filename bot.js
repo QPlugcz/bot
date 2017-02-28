71d7413823c74111455fd4290e85557c7bcd439a
Hellbyte <false.ying@gmail.com>
2017-02-28 17:56:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3178,20 +3178,6 @@ setminihryCommand: {
                 }
             },
             
-            awayCommand: {
-                command: ['away', 'afk'],
-                rank: 'user',
-                type: 'startsWith',
-                functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    else {
-                        
-                        API.sendChat("[@" + chat.un + "] Si preč od klávesnice: " + chat.message.substr(cmd.length + 1));
-                    }
-                }
-            },
-            
             naborCommand: {
                 command: 'nabor',
                 rank: 'user',
